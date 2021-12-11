a41772f90bc80a7ac57768ece2798b141e3f3ed0
DDD <77850088+D-R-D@users.noreply.github.com>
2021-12-11 13:57:29 +0900

    テストコード

---
+++ mctest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace mctest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, j = 0;
+            List<string> list;
+
+            Task.Run(() =>
+            {
+                MCex.MCSearch.MulticastResponser("224.100.100.100", "192.168.11.147", 1000, 1001);
+                Thread.Sleep(2100);
+            });
+
+            while (i < 100)
+            {
+                Thread.Sleep(100);
+                list = MCex.MCSearch.MulticastSercher("224.100.100.100", "192.168.11.147", 1000, 1001, 10);
+
+                if (list.Count != 0)
+                {
+                    foreach (var str in list)
+                    {
+                        Console.WriteLine(str);
+                        j++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("no items");
+                }
+                i++;
+            }
+
+            Console.WriteLine("Collect " + j + "/" + i);
+        }
+    }
+}
